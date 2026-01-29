60aa11fba74254043773ee7b8da52acefbc5baba
unknown <siddhesh.agarwal@gmail.com>
2026-01-30 00:48:30 +0530

    used human-format for stargazers count

--- components/github-link.tsx
+++ components/github-link.tsx
@@ -1,20 +1,21 @@
 import Link from "next/link";
 import { Button } from "@/registry/new-york/components/button";
 import { Icons } from "@/registry/new-york/components/icons";
+import humanFormat from "human-format";
 
 export function GitHubLink() {
   const { username, repo } = { username: "Siddhesh-Agarwal", repo: "sid-cn" };
   return (
-    <Button asChild size="sm" variant="ghost" className="h-8 shadow-none">
-      <Link
-        href={`https://github.com/${username}/${repo}`}
-        target="_blank"
-        rel="noreferrer"
-      >
+    <Link
+      href={`https://github.com/${username}/${repo}`}
+      target="_blank"
+      rel="noreferrer"
+    >
+      <Button size="sm" variant="ghost">
         <Icons.gitHub />
         <StarsCount username={username} repo={repo} />
-      </Link>
-    </Button>
+      </Button>
+    </Link>
   );
 }
 
@@ -37,11 +38,13 @@ export async function StarsCount({
 }) {
   const json = await getStarsCount(username, repo);
 
+  if (!json.stargazers_count) return null;
+
   return (
     <span className="text-muted-foreground text-xs tabular-nums">
-      {json.stargazers_count >= 1000
-        ? `${(json.stargazers_count / 1000).toFixed(1)}k`
-        : json.stargazers_count.toLocaleString()}
+      {humanFormat(json.stargazers_count, {
+        maxDecimals: 1,
+      })}
     </span>
   );
 }
